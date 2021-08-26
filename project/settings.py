54f4788fab66c88eb4e8dd0e2e8e476e74877d61
Edgar Santana <santanaprogrammer@gmail.com>
2021-08-26 19:11:15 -0400

    update

--- project/settings.py
+++ project/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from project.secrets import SECRET_KEY
+from secrets import *
 from pathlib import Path
 
-from secrets import *
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
+SECRET_KEY = str(('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
