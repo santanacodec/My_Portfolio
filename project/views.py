300f1768c742d1386cbe2b9b95f2477d88d823ee
Edgar Santana <santanaprogrammer@gmail.com>
2021-08-31 17:44:52 -0400

    update

--- project/views.py
+++ project/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def contact(request):
+    return render(request, 'contact.html')
