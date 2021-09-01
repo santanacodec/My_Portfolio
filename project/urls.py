8a353df37644e0e1149f7d32c063f8548decd5c3
Edgar Santana <santanaprogrammer@gmail.com>
2021-08-31 23:35:54 -0400

    updated paths

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('portfolio', views.portfolio, name="portfolio"),
     path('contact', views.contact, name="contact")
 ]
--- project/views.py
+++ project/views.py
@@ -2,11 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
-
-def portfolio(request):
-    return render(request, 'portfolio.html')
-
 def contact(request):
     return render(request, 'contact.html')
