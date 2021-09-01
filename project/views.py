134a980a7e91f57d89dff26bdad09061c4ed376f
Edgar Santana <santanaprogrammer@gmail.com>
2021-09-01 14:09:10 -0400

    update

--- project/views.py
+++ project/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.template import loader
+#from django.http import HttpResponse
+#from django.template import loader
 from django.shortcuts import render
 
 def contact(request):
