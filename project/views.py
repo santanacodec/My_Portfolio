174c24df63197d253a67edbd592ca9df8f6927ad
Edgar Santana <santanaprogrammer@gmail.com>
2021-09-07 17:27:26 -0400

    moved files

--- project/views.py
+++ project/views.py
@@ -2,5 +2,11 @@
 #from django.template import loader
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'index.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
 def contact(request):
     return render(request, 'contact.html')
