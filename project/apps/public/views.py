cf9a54c862fe32600477a41b8955fb36852b8c91
Edgar Santana <santanaprogrammer@gmail.com>
2021-09-27 23:41:28 -0400

    started apps

--- project/apps/public/views.py
+++ project/apps/public/views.py
@@ -2,7 +2,6 @@
 #from django.template import loader
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html')
--- project/urls.py
+++ project/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('portfolio', views.portfolio, name="portfolio"),
-    path('contact', views.contact, name="contact")
+    path("admin/", admin.site.urls),
+    path("", include('project.apps.public.urls')),
 ]
